7f5810f2ec1192ba3e3495bc3cec1a564ba4b940
PrachiSh <prachisharma.eq@gmail.com>
2016-06-13 05:55:16 +0530

    module 4 1 complete

--- 04/mod4q1.rkt
+++ 04/mod4q1.rkt
@@ -18,18 +18,18 @@
 (define RECX 200)
 (define RECY 100)
 
-(define RECVEL-X -12)
-(define RECVEL-Y 20)
+(define RECVEL-X 1)
+(define RECVEL-Y 1)
 
 (define (main initial-pos)
   (big-bang (initial-world initial-pos)
+            (on-tick world-after-tick 1)
             (on-mouse world-after-mouse-event)
             (on-key world-after-key-event)
             (on-draw draw-world)))
 
 (define REC-IMAGE (rectangle RECW RECH "outline" "blue"))
-(define CIR-IMAGE (circle 30 "outline" "red"))
-(define ELP-IMAGE (ellipse 30 60 "solid" "blue"))
+(define CIR-IMAGE (circle 30 "solid" "red"))
 
 (define HALF-REC-WIDTH  (/ (image-width  REC-IMAGE) 2))
 (define HALF-REC-HEIGHT (/ (image-height REC-IMAGE) 2))
@@ -40,29 +40,87 @@
 
 (define-struct world (LOR))
 
-(define-struct rec( REC-IMAGE x-pos y-pos vel-x vel-y selected?))
+(define-struct rec( REC-IMAGE x-pos y-pos vel-x  vel-y is-selected?))
+
 
 (define (initial-world w)
  (make-world  empty))
 
 (define (world-after-key-event w k)
-  (cond [(key=? k "n" ) (updated-world  w)]))
+  (cond [(key=? k "n" ) (updated-world w)]
+        [else (recvel-after-key-event w k)]))
+
+(define (recvel-after-key-event w k)
+  (make-world
+   (velocity-after-key-event (world-LOR w) k)))
+
+(define (velocity-after-key-event l k)
+  (cond[(empty? l) empty]
+       [else (cons (lorvel-after-key-event (first l) k) (velocity-after-key-event (rest l) k))]))
+
+(define (lorvel-after-key-event r k)
+  (cond[(key=? k "up") (in-y-vel r)]
+       [(key=? k "down") (de-y-vel r)]
+       [(key=? k "left") (de-x-vel r)]
+       [(key=? k "right") (in-x-vel r)]))
+
+(define (in-y-vel r)
+  (if (rec-is-selected? r)
+       (make-rec  REC-IMAGE (rec-x-pos r) (rec-y-pos r) (rec-vel-x r) (+ (rec-vel-y r) 2) true)
+      r))
+
+(define (de-y-vel r)
+  (if (rec-is-selected? r)
+  (make-rec REC-IMAGE (rec-x-pos r) (rec-y-pos r) (rec-vel-x r) (- (rec-vel-y r) 2) true)
+  r))
+
+ 
+(define (de-x-vel r)
+  (if (rec-is-selected? r)
+  (make-rec REC-IMAGE (rec-x-pos r) (rec-y-pos r) (- (rec-vel-x r) 2) (rec-vel-y r) true)
+  r))
+
+(define (in-x-vel r)
+  (if (rec-is-selected? r)
+   (make-rec REC-IMAGE (rec-x-pos r) (rec-y-pos r) (+ (rec-vel-x r) 2) (rec-vel-y r) true)
+  r))
+
 
 (define (updated-world w)
   (make-world
-   (cons (make-rec REC-IMAGE (/ CANVAS-WIDTH 2) (/ CANVAS-HEIGHT 2) 0 0 true) (world-LOR w))))
+   (cons (make-rec REC-IMAGE (/ CANVAS-WIDTH 2) (/ CANVAS-HEIGHT 2) 0 0 false) (world-LOR w))))
 
 (define (draw-world w)
-  (draw-list (world-LOR w )))
+  (draw-list (world-LOR w)))
 
 (define (draw-list l)
   (cond [(empty? l) empty-canvas]
-        [(equal? (length l) 1) (place-image CIR-IMAGE 100 100 empty-canvas)]
-        [else (place-image ELP-IMAGE 150 100 empty-canvas)]))
+        [else  ( place-image (overlay (text (string-append (number->string (rec-vel-x (first l))) (number->string (rec-vel-y (first l)))) 10 "red") REC-IMAGE) (rec-x-pos (first l)) (rec-y-pos (first l)) (draw-list (rest l)))]))
+
+(define (world-after-tick w )
+  (make-world
+   (lor-after-tick (world-LOR w))))
+
+(define (lor-after-tick l)
+  (cond[(empty? l) empty]
+       [else (cons (rec-after-tick (first l)) (lor-after-tick (rest l)))]))
+
+(define (rec-after-tick r)
+  (cond [(rec-is-selected? r) (make-rec REC-IMAGE (rec-x-pos r) (rec-y-pos r) (rec-vel-x r) (rec-vel-y r) true)]
+     [(or (< (rec-x-pos r) (/ RECW 2)) ( > (rec-x-pos r) (- CANVAS-WIDTH (/ RECW 2))))  (make-rec REC-IMAGE
+         (+ (rec-x-pos r) (* (rec-vel-x r) -1)) (rec-y-pos r) (* (rec-vel-x r) -1) (rec-vel-y r) false)]
+        [(or (< (rec-y-pos r) (/ RECH 2)) (> (rec-y-pos r) (- CANVAS-HEIGHT (/ RECH 2)))) (make-rec REC-IMAGE
+           (rec-x-pos r) (+ (rec-y-pos r) (* (rec-vel-y r) -1)) (rec-vel-x r) (* (rec-vel-y r) -1) false )]
+        [else (make-rec REC-IMAGE
+   (+ (rec-x-pos r) (rec-vel-x r))
+   (+ (rec-y-pos r) (rec-vel-y r))
+    (rec-vel-x r)
+   (rec-vel-y r) false)]))
+  
 
  
-(define (place-rec c)
-  (place-image REC-IMAGE (rec-x-pos c) (rec-y-pos c) empty-canvas))
+;;(define (place-rec c)
+  ;;(place-image REC-IMAGE (rec-x-pos c) (rec-y-pos c) empty-canvas))
 
  
 (define (world-after-mouse-event w mx my mev)
@@ -72,7 +130,7 @@
 
 (define (lor-after-mouse-event l mx my mev)
   (cond [(empty? l) empty]
-   [else (cons (rec-after-mouse-event (first l)mx my mev ) (lor-after-mouse-event (rest l) mx my mev))]))
+   [else (cons (rec-after-mouse-event (first l) mx my mev ) (lor-after-mouse-event (rest l) mx my mev))]))
 
 (define (rec-after-mouse-event r mx my mev)
   (cond
@@ -82,18 +140,17 @@
     [else r]))
 
 (define (rec-after-button-down r x y)
-  (if (in-rec? r x y)
-      (make-rec  REC-IMAGE (rec-x-pos r) (rec-y-pos r) (rec-vel-x r) (rec-vel-y r) true)
-      r))
+  (cond [(in-rec? r x y) (make-rec  REC-IMAGE (rec-x-pos r) (rec-y-pos r) (rec-vel-x r) (rec-vel-y r) true)]
+        [else r]))
 
 (define (rec-after-drag r x y)
-  (cond [(rec-selected? r)
+  (cond [(rec-is-selected? r)
       (make-rec REC-IMAGE x y (rec-vel-x r) (rec-vel-y r) true)]
       [else r]))
 
 (define (rec-after-button-up r x y)
-  (if (rec-selected? r)
-      (make-rec REC-IMAGE x y (rec-vel-x r) (rec-vel-y r)  true)
+  (if (rec-is-selected? r)
+      (make-rec REC-IMAGE x y (rec-vel-x r) (rec-vel-y r) false)
       r))
 
 (define (in-rec? r x y)
